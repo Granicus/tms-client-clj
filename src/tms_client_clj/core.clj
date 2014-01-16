90a7912170c5d4e4b179ba5015379aa4366aabab
Tyler Holien <tyler.holien@govdelivery.com>
2014-01-16 12:10:12 -0600

    initial commit

---
+++ src/tms_client_clj/core.clj
@@ -0,0 +1,8 @@
+(ns tms-client-clj.core
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
