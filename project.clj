409af35162f222ffdd0d8ceb10b7d237f6679bcb
Chris Thompson <chris.thompson@govdelivery.com>
2014-02-06 12:39:23 -0600

    add environment credentials using environ

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject tms-client-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/govdelivery/tms-client-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.6"]
-                 [org.clojure/data.json "0.2.2"]])
+                 [org.clojure/data.json "0.2.2"]
+                 [environ "0.4.0"]])
--- src/tms_client_clj/core.clj
+++ src/tms_client_clj/core.clj
@@ -1,6 +1,12 @@
 (ns tms-client-clj.core
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
+             [clojure.data.json :as json]
+             [environ.core :as env] ))
+
+(def tms-creds
+  { :auth-token (env/env :auth-token)
+    :api-root   (env/env :api-root) })
+
 
 (defn foo
   "I don't do a whole lot."
